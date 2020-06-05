f108b22edef7af7e08a6084eb4f87c08ee650667
Mario Silva <mario.silvae@outlook.com>
2020-06-05 14:09:04 -0500

    Giving fuctionality to the gameboard with functions to illuminate in a random way the gameboard

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,62 @@
+const blue = document.getElementById('blue');
+const violet = document.getElementById('violet');
+const orange = document.getElementById('orange');
+const green = document.getElementById('green');
 const btnStart = document.getElementById('btnStart');
 
 class Game {
     constructor() {
         this.start();
+        this.generateSequence();
+        this.nextLevel();
     }
 
     start() {
         btnStart.classList.add('hide');
+        this.level = 1;
+        this.colors = {
+            blue: blue,
+            violet: violet,
+            orange: orange,
+            green: green,
+        }
+    }
+
+    generateSequence() {
+        this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    }
+
+    nextLevel() {
+        this.illuminateSequence();
+    }
+
+    transformNumberToColor(number) {
+        switch(number){
+            case 0:
+                return 'blue'
+            case 1:
+                return 'violet'
+            case 2:
+                return 'orange'
+            case 3:
+                return 'green'
+        }
+    }
+
+    illuminateSequence() {
+        for (let i = 0; i < this.nivel; i++) {
+            const color = this.transformNumberToColor(this.sequence[i]);
+            setTimeout(() => this.illuminateColor(color), 1000 * i);
+        }
+    }
+
+    illuminateColor(color) {
+        this.colors[color].classList.add('light');
+        setTimeout(()=> this.turnOffColor(color), 350);
+    }
+
+    turnOffColor(color) {
+        this.colors[color].classList.remove('light');
     }
 }
 
